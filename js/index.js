1cc07b9a791204ede989781d7b525a4b72f9cb07
Ribeiro1300 <59618809+Ribeiro1300@users.noreply.github.com>
2021-06-11 12:11:47 -0300

    2.0
    Primeiro modelo pronto

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-document.query
+function exec()
+{
+    var display = document.querySelector("h1")
+    var n1 = Math.floor(Math.random() * 6)+1
+    var n2 = Math.floor(Math.random() * 6)+1
+    var x = document.querySelector("#img1")
+    var y = document.querySelector("#img2")
+    x.setAttribute("class", "img"+n1)
+    y.setAttribute("class", "img"+n2)
+
+    if (n1>n2)
+    {
+        display.innerHTML = "Player 1 Ganhou!"
+    }
+    else if (n1<n2)
+    {
+        display.innerHTML = "Player 2 Ganhou!"
+    }
+    else if (n1 == n2)
+        display.innerHTML = "Deu empate!"
+
+
+
+}
